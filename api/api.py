26f7d6a7a64eefa6cf21f8b24a51a61c9e3b7313
Daniel Soares <danielsoares.data@outlook.com>
2024-04-15 16:19:02 -0300

    Finalizando a estrutura de predição da API

--- api/api.py
+++ api/api.py
@@ -1,9 +1,17 @@
-#import joblib
+import joblib
+import uvicorn
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-#import pandas as pd
+from src.build_features.preprocess import (
+    make_continents,
+    make_time_based_cols,
+    fill_nans,
+    drop_unused_cols
+)
 
 api = FastAPI()
+
 
 # Definindo o tipo padrão das variáveis conforme os dados raw
 class Transacao(BaseModel):
@@ -27,13 +35,45 @@ class Transacao(BaseModel):
     data_compra: object
     valor_compra: float
 
+
 # Definindo a API
-@api.post("/prever")  
+@api.post("/prever")
 def prever_fraude(transacao: Transacao):
-    print(transacao)
     
-    return {"mensagem": "Transacao concluída com sucesso"}
+    # Carregando o modelo
+    pipeline = joblib.load("models/pipe.pkl")
+    
+    # Carregando os dados no formato json
+    json = transacao.model_dump()
+
+    # Convertendo em dataframe
+    data = pd.DataFrame.from_dict([json])
+
+    # Criando a coluna 'continente'
+    data["continente"] = make_continents(data, "pais")
+     
+    # Criando as colunas baseadas na coluna datetime
+    data = make_time_based_cols(data, "data_compra") 
+    
+    # Definindo as colunas para preenchimentop de missing
+    cols_to_fill = ["entrega_doc_1", "entrega_doc_2", "entrega_doc_3"] 
+    
+    # Preenchendo o missing
+    data[cols_to_fill] = fill_nans(data, cols_to_fill) 
+    
+    # Definindo as colunas para dropar
+    cols_to_drop = ["produto", "data_compra"] 
+    
+    # Dropando as colunas
+    data = drop_unused_cols(data, cols_to_drop)
+    print("As colunas indo para a previsão são:", data.columns)
+    
+    # Calculando a previsão
+    prob = (pipeline.predict_proba(data)[:, 1] >= 0.66).astype(int)
+    
+    return {"Predicao": str(prob)}
+
 
 if __name__ == "__main__":
-    import uvicorn
+
     uvicorn.run(api, host="0.0.0.0", port=8000)
